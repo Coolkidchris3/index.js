0d5f4ab6d0ff13b93d88421e6ca3abc1627603ca
Coolkidchris3 <101761765+Coolkidchris3@users.noreply.github.com>
2022-03-17 16:26:28 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+var password = "gobama2010chris2010"
+
+function passcheck() {
+
+if(document.getElementById('pass1').valule != password) {
+alert('Wrong password idot and your someone in my class');   
+} 
+
+if(document.getElementById('pass1').valule == password) {
+   alert('Welcome Christopher Pelz');   
+}  
+}
